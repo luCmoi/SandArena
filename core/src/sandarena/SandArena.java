00d47560f8505a9134e3c997359cf3b5465dfb87
luCmoi <lucmoua@gmail.com>
2015-07-18 19:30:19 +0200

    Serie test1 XML

--- core/src/sandarena/SandArena.java
+++ core/src/sandarena/SandArena.java
@@ -1,14 +1,23 @@
 package sandarena;
 
 import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
+import org.w3c.dom.Document;
+import org.xml.sax.ErrorHandler;
+import org.xml.sax.SAXException;
+import org.xml.sax.SAXParseException;
+
 import java.io.IOException;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
 
 import sandarena.donnee.BanqueCompetence;
 import sandarena.donnee.BanquePersonnage;
 import sandarena.donnee.BanqueSol;
-import sandarena.joueur.Personnage;
 import sandarena.partie.gui.ScreenPartie;
 
 /**
@@ -35,12 +44,49 @@ public class SandArena extends Game {
         } catch (IOException ex) {
             System.err.println("error : File");
         }
-        new Personnage("Barbare des Sables");
+        //
+        testXML();
+        //
         this.screenPartie = new ScreenPartie(this);
         this.setScreen(screenPartie);
     }
 
     public SpriteBatch getBatch() {
         return this.batch;
+    }
+
+    public void testXML() {
+        DocumentBuilderFactory domFactory = DocumentBuilderFactory.newInstance();
+        domFactory.setValidating(true);
+        DocumentBuilder builder = null;
+        try {
+            builder = domFactory.newDocumentBuilder();
+        } catch (ParserConfigurationException e) {
+            e.printStackTrace();
+        }
+        builder.setErrorHandler(new ErrorHandler() {
+            @Override
+            public void error(SAXParseException exception) throws SAXException {
+                // do something more useful in each of these handlers
+                exception.printStackTrace();
+            }
+
+            @Override
+            public void fatalError(SAXParseException exception) throws SAXException {
+                exception.printStackTrace();
+            }
+
+            @Override
+            public void warning(SAXParseException exception) throws SAXException {
+                exception.printStackTrace();
+            }
+        });
+        try {
+            Document doc = builder.parse(Gdx.files.internal("XML/competence.xml").file());
+        } catch (SAXException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
